0897c18cf0846eb82399f253795d4a324f682d9f
Lucas Freitas de Barros <lucasfreitasdebarros@gmail.com>
2019-07-17 18:19:35 -0300

    qt5 updated

--- qt5.c
+++ qt5.c
@@ -2,86 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define EXP_MAX_SIZE 1024
+#define STACK_SIZE 300
 
-int prec(char operator);
-void paraPosfixa(const char *exp, char *out);
+struct Stack{
+	char v[STACK_SIZE];
+	int top;
+};
 
+void push(struct Stack* s, char c);
+char pop(struct Stack* s);
+void clear(struct Stack* s);
 
 int main (void) {
-
-	int N;
-	char infixa[EXP_MAX_SIZE], posfixa[EXP_MAX_SIZE];
 	
-	scanf("%d\n", &N);
 	
-	while (N-- > 0) {
-		fgets(infixa, EXP_MAX_SIZE, stdin);
-		paraPosfixa(infixa, posfixa);
-		printf("%s\n", posfixa);
-	} 
-	
-	return 0;
 }
 
-
-
-
-void paraPosfixa(const char *exp, char *out) {
-	char p[EXP_MAX_SIZE];
-	int i, o = 0, t = -1;
-	char c;
-	
-	for (i = 0; exp[i] != '\n' && exp[i] != '\0'; i++) {
-		c = exp[i]; 
-		if (c == ' ') {
-			continue;
-		}else if (c == '(') {
-			p[++t] = c;
-		}else if (c == ')') {
-			while (t > -1 && p[t] != '(') {
-				out[o++] = p[t--];
-			}
-			t--;
-		}else if ((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-			out[o++] = c;
-		}else {
-			while (t > -1 && p[t] != '(' && prec(p[t]) >= prec(c)) {
-				out[o++] = p[t--];
-			}
-			p[++t] = c;
-		} 
-	
+void push(struct Stack* s, char c){
+	if (s->top < (STACK_SIZE)){
+		s->top++;
+		s->v[s->top] = c;
+	}else{
+		exit(1);
 	}
-	while (t > -1){
-		out[o++] = p[t--];	
-	} 
-	out[o++] = '\0';
 }
 
-
-
-
-
-int prec(char operator) {
-	switch (operator) {
-		case '^':
-			return 3;
-
-		case '*':
-			return 2;
-
-		case '/':
-			return 2;
-
-		case '+':
-			return 1;
-
-		case '-':
-			return 1;
-
-		default:
-			return 0;
-
+char pop(struct Stack* s){
+	if (s->top >= 0){
+		char tmp = s->v[s->top];
+		s->top--;
+		return tmp;
+	}else{
+		
+		return NULL;
 	}
+}
+
+void clear(struct Stack* s){
+	do{
+		
+	}while(pop(s) != NULL);
 }
